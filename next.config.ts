ee5913bbeec82a95942ec155451ee81b07e6b921
josh <apexofcheese@gmail.com>
2026-04-28 18:04:52 +0100

    chore: next.config cross-origin to framer

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "frame-ancestors 'self' https://joshuakon.framer.website/ https://*.framer.website https://framer.website;",
+          },
+          {
+            key: "Cross-Origin-Resource-Policy",
+            value: "cross-origin",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
